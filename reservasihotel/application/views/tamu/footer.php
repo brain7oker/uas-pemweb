ade90486633519f9f0671d2b654cb66a9905d1ae
BrainJoker <charliefrederico7@gmail.com>
2021-12-03 20:51:39 +0700

    css footer

--- reservasihotel/application/views/tamu/footer.php
+++ reservasihotel/application/views/tamu/footer.php
@@ -2,7 +2,7 @@
 <footer >
   <div class="container mx-auto">
     <div class="copyright text-center my-auto">
-      <span>Copyright © <a href="https://www.sarjanakomedi.com"><?php echo SITE_NAME ."</a> ". Date('Y') ?></span>
+      <span>Copyright CCFH<?php "</a> ". Date('Y') ?></span>
     </div>
   </div>
 </footer>
