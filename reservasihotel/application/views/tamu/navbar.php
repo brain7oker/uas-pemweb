75899eeaa7a657621d4904dfea9ad714d0932028
henrylaufrans <henry.laufrans@student.umn.ac.id>
2021-12-04 15:50:07 +0700

    Update navbar.php

--- reservasihotel/application/views/tamu/navbar.php
+++ reservasihotel/application/views/tamu/navbar.php
@@ -3,7 +3,7 @@
     <a class="navbar-brand mr-1" href="<?php echo site_url('Welcome') ?>"><?php echo SITE_NAME ?></a>
 
     <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
-      <i class="fas fa-bars"></i>
+      
     </button>
 
     <!-- Navbar Search -->
